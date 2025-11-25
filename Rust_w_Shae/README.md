3d601b812466a85fd6b99d404b803a857a6e23ba
May-Lee <mayleesia@gmail.com>
2025-11-25 18:00:32 +0100

    Add README

---
+++ Rust_w_Shae/README.md
@@ -0,0 +1,13 @@
+# Rust project w/ Shae
+Goals are:
+- Download a text file from Project Gutenberg
+   -[x] Manually download and move into project folder
+- Cut it into words
+   -[x] Get word count
+- Figure out frequency of words
+   -[ ] Group words and count
+   -[ ] Order the top ten words
+- Final touches
+   -[ ] Print a list of the top ten words
+   -[ ] Take user input and download the file into the project folder
+
--- Rust_w_Shae/src/main.rs
+++ Rust_w_Shae/src/main.rs
@@ -1,9 +1,14 @@
+//Including methods from standard library
 use std::fs::File;
 use std::io::Read;
 
 fn main() {
-    let mut f = File::open	("pg26.txt").unwrap();
+    //unwrap() says don't compile if there's an issue
+    let mut f = File::open("pg26.txt").unwrap();
+    // Vec is a data structure that is dynamic and stored sequentially
     let mut buffer = Vec::new();
-    // f.read_to_end(&mut f).
+    // "unpack" the file to the vec
     f.read_to_end(&mut buffer).unwrap();
+    // print out the word count
+    println!("There are {} words in your text.", buffer.len());
 }
